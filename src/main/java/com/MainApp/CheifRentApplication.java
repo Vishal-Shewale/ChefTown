f336a42d2db47695f6d46b0c2ac7cbffb7e448b9
FigherJet <128166620+FigherJet@users.noreply.github.com>
2025-01-10 15:45:18 +0530

    update in the command

--- src/main/java/com/MainApp/CheifRentApplication.java
+++ src/main/java/com/MainApp/CheifRentApplication.java
@@ -9,6 +9,7 @@ public class CheifRentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CheifRentApplication.class, args);
 		//Hii these is the common project of the Chef
+		//Tu kharach yeda ganda aaahe
 	}
 
 }
